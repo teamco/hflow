9b4fa518ca2c04e45fbe03d2915fec97b8f48f75
Vladimir Tkach <teamco@gmail.com>
2021-11-29 00:08:21 +0200

    start search

---
+++ src/pages/landing/sections/content/search.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import classnames from 'classnames';
+
+import styles from 'pages/landing/landing.module.less';
+
+const Search = props => {
+  const { t, className } = props;
+
+  const bottomProps = {
+    direction: 'bottom',
+    dims: {
+      left: { width: '45vw' },
+      right: { width: '15vw' },
+      top: { width: '15vh', color: '#fff', style: 'solid' }
+    }
+  };
+
+  return (
+      <div className={styles.searchWrapper}>
+        <div className={classnames(className, styles.search)}>
+
+
+        </div>
+      </div>
+  );
+};
+
+export default withTranslation()(Search);
