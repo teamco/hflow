1dcbbd9e34cc75f4cbd4dcbbb28ffe3e4890ab56
belkinbelkin <ellerin5770@gmail.com>
2022-01-20 17:46:08 +0200

    resolution
    - removed wait
    - made a const

--- cypress/integration/landing.spec.js
+++ cypress/integration/landing.spec.js
@@ -6,12 +6,12 @@ describe('Landing Page', () => {
     });
 
     it('Renders login page', () => {
-        cy.wait(5000);
         cy.get('.ant-tabs-nav-list').should('be.visible');
-        cy.get('.ant-tabs-tab-btn').first().should('have.text', 'Real Estate');
-        cy.get('.ant-tabs-tab-btn').eq(1).should('have.text', 'Lawyers');
-        cy.get('.ant-tabs-tab-btn').eq(2).should('have.text', 'Consultants');
-        cy.get('.ant-tabs-tab-btn').eq(3).should('have.text', 'Agents');
-        cy.get('.ant-tabs-tab-btn').eq(4).should('have.text', 'Banks');
+        const selectedButton = cy.get('.ant-tabs-tab-btn');
+        selectedButton.first().should('have.text', 'Real Estate');
+        selectedButton.eq(1).should('have.text', 'Lawyers');
+        selectedButton.eq(2).should('have.text', 'Consultants');
+        selectedButton.eq(3).should('have.text', 'Agents');
+        selectedButton.eq(4).should('have.text', 'Banks');
     });
 });
